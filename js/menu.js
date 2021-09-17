1ab5f72d89d00b1f20ddb0bdcaf2db7736bdad6f
Carlos Gallardo <carlitos.gallardo08@hotmail.com>
2021-09-16 23:15:53 -0500

    first commit

---
+++ js/menu.js
@@ -0,0 +1,43 @@
+let btnMenu = document.getElementById("btn-menu");
+let menu = document.getElementById("menu");
+
+btnMenu.addEventListener("click",() => {
+    menu.classList.toggle("mostrar");
+});
+
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider__section");
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+const btnLeft = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function next() {
+    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(() => {
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+        slider.style.marginLeft = "-100%";
+    },500);
+}
+function prev() {
+    let sliderSection = document.querySelectorAll(".slider__section");
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    slider.style.marginLeft = "0";
+    slider.style.transition = "all 0.5s";
+    setTimeout(() => {
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+        slider.style.marginLeft = "-100%";
+    },500);
+}
+btnRight.addEventListener("click", () => {
+    next()
+});
+btnLeft.addEventListener("click", () => {
+    prev()
+});
